21d3b94c1b79afd27ac64c3abe73d6b5a60b9703
aicsoft115 <alexahn8855@gmail.com>
2019-06-14 03:25:06 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react'
+import Test from './components/Test'
+
+import './index.css'
+
+const styles = {
+  network: {
+    display: 'inline-block',
+    width: '500px',
+    height: '500px'
+  },
+  report: {
+    display: 'inline-block',
+    float: 'right',
+    width: '350px',
+    verticalAlign: 'top',
+    backgroundColor: '#efefef',
+    marginTop: '1rem'
+  }
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <div style={{marginLeft: 'auto', marginRight: 'auto', width: '500px'}}>
+        <Test style={styles.network} />
+
+        <div style={styles.report}>
+          <ul style={{listStyle: 'none'}}>
+            <li style={{marginBottom: '0.5rem'}}>
+              <div style={{display: 'table'}}>
+                <div className="group-circle" style={{border: '0.3rem solid #d35400'}}></div>
+                <b style={{display: 'table-cell', verticalAlign: 'middle', paddingLeft: '1rem'}}>Trader</b>
+              </div>
+            </li>
+            <li style={{marginBottom: '0.5rem'}}>
+              <div style={{display: 'table'}}>
+                <div className="group-circle" style={{border: '0.3rem solid #2980b9'}}></div>
+                <b style={{display: 'table-cell', verticalAlign: 'middle', paddingLeft: '1rem'}}>Sale</b>
+              </div>
+            </li>
+            <li style={{marginBottom: '0.5rem'}}>
+              <div style={{display: 'table'}}>
+                <div className="group-rectangle" style={{border: '0.3rem solid #16a085'}}></div>
+                <b style={{display: 'table-cell', verticalAlign: 'middle', paddingLeft: '1rem'}}>Within CIMB</b>
+              </div>
+            </li>
+            <li style={{marginBottom: '0.5rem'}}>
+              <div style={{display: 'table'}}>
+                <div className="group-rectangle" style={{border: '0.3rem solid #c0392b'}}></div>
+                <b style={{display: 'table-cell', verticalAlign: 'middle', paddingLeft: '1rem'}}>External - Interbank</b>
+              </div>
+            </li>
+            <li style={{marginBottom: '0.5rem'}}>
+              <div style={{display: 'table'}}>
+                <div className="group-rectangle" style={{border: '0.3rem solid #f39c12'}}></div>
+                <b style={{display: 'table-cell', verticalAlign: 'middle', paddingLeft: '1rem'}}>External - Broker</b>
+              </div>
+            </li>
+            <li style={{marginBottom: '0.5rem'}}>
+              <div style={{display: 'table'}}>
+                <div className="group-rectangle" style={{border: '0.3rem solid #6112f3'}}></div>
+                <b style={{display: 'table-cell', verticalAlign: 'middle', paddingLeft: '1rem'}}>External - Client (Corporate)</b>
+              </div>
+            </li>
+            <li style={{marginBottom: '0.5rem'}}>
+              <div style={{display: 'table'}}>
+                <div className="group-rectangle" style={{border: '0.3rem solid #cb12f3'}}></div>
+                <b style={{display: 'table-cell', verticalAlign: 'middle', paddingLeft: '1rem'}}>External - Client (Non-Bank FI)</b>
+              </div>
+            </li>
+            <li style={{marginBottom: '0.5rem'}}>
+              <div style={{display: 'table'}}>
+                <div className="group-pentagon"></div>
+                <b style={{display: 'table-cell', verticalAlign: 'middle', paddingLeft: '1rem'}}>Unknown</b>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/components/Test.js
@@ -0,0 +1,170 @@
+import React, { Component } from "react";
+import Network from "vis-react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faDownload, faExpand } from "@fortawesome/free-solid-svg-icons"
+
+import '../index.css'
+import jsonData from "../data.json";
+
+const options = {
+  autoResize: true,
+  width: '500',
+  height: '500',
+  nodes: {
+    shape: 'circularImage',
+    fixed: true,
+    font: {
+      size: 14,
+      color: 'black',
+      vadjust: -78
+    },
+    physics: false,
+    size: 25,
+    borderWidth: 6
+  },
+  edges: {
+    width: 5,
+    arrows: {
+      to:     {enabled: true, scaleFactor:0.1, type:'arrow'},
+      from:   {enabled: false, scaleFactor:0.1, type:'arrow'}
+    },
+    arrowStrikethrough: false,
+    chosen: true,
+    font: {
+      size: 14, // px
+      face: 'arial',
+      background: '#ffffff',
+      strokeWidth: 0, // px
+      strokeColor: '#8d8d8d',
+      multi: 'html'
+    },
+    labelHighlightBold: true,
+    physics: false,
+    selectionWidth: 1,
+    widthConstraint: false
+  },
+  groups: {
+    "trader": {
+      shape: 'circularImage',
+      size: 25,
+      color: {
+        background: '#ffffff', border: '#d35400'
+      },
+      font: {multi: 'html'}
+    },
+    "sale": {
+      shape: 'circularImage',
+      size: 25,
+      color: {
+        background: '#ffffff', border: '#2980b9'
+      },
+      font: {multi: 'html'}
+    },
+    "within-cimb": {
+      shape: 'image',
+      shapeProperties: {useBorderWithImage: true},
+      size: 23,
+      borderWidth: 3,
+      color: {
+        background: '#ffffff', border: '#16a085'
+      },
+      font: {multi: 'html'}
+    },
+    "ex-interbank": {
+      shape: 'image',
+      shapeProperties: {useBorderWithImage: true},
+      size: 23,
+      borderWidth: 3,
+      color: {
+        background: '#ffffff', border: '#c0392b'
+      },
+      font: {multi: 'html'}
+    },
+    "ex-broker": {
+      shape: 'image',
+      shapeProperties: {useBorderWithImage: true},
+      size: 23,
+      borderWidth: 3,
+      color: {
+        background: '#ffffff', border: '#f39c12'
+      },
+      font: {multi: 'html'}
+    },
+    "ex-client-corporate": {
+      shape: 'image',
+      shapeProperties: {useBorderWithImage: true},
+      size: 23,
+      borderWidth: 3,
+      color: {
+        background: '#ffffff', border: '#6112f3'
+      },
+      font: {multi: 'html'}
+    },
+    "ex-client-non-bank-fi": {
+      shape: 'image',
+      shapeProperties: {useBorderWithImage: true},
+      size: 23,
+      borderWidth: 3,
+      color: {
+        background: '#ffffff', border: '#cb12f3'
+      },
+      font: {multi: 'html'}
+    },
+    "unknown": {
+      shape: 'image',
+      shapeProperties: {useBorderWithImage: true},
+      size: 23,
+      borderWidth: 3,
+      color: {
+        background: '#ffffff', border: '#6f6f6f'
+      },
+      font: {multi: 'html'}
+    }
+  }
+}
+
+export default class Test extends Component {
+  setState(stateObj) {
+    if (this.mounted) {
+      super.setState(stateObj);
+    }
+  }
+  componentWillMount() {
+    this.mounted = true;
+  }
+  constructor(props) {
+    super(props);
+
+    let newGraph = {};
+    newGraph.nodes = jsonData.nodes;
+    newGraph.edges = jsonData.edges;
+
+    this.state = {
+      graph: newGraph,
+      style: { width: "100%", height: "100%" },
+      network: null
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{height: 'auto', overflow: 'hidden'}}>
+          <div className="vis-react-title" style={{marginLeft: '2rem'}}><h3>Dealer's networks</h3></div>
+          <div className="vis-react-toolbar">
+            <FontAwesomeIcon style={{color: '#790307'}} icon={faDownload} />
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <FontAwesomeIcon style={{color: '#790307'}} icon={faExpand} />
+          </div>
+        </div>
+        <Network
+          graph={this.state.graph}
+          style={this.state.style}
+          options={options}
+          vis={vis => (this.vis = vis)}
+        />
+      </div>
+    );
+  }
+}
